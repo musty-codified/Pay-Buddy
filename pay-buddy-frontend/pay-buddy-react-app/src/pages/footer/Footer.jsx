ea5f444369ee1cd662c53e4f5e5573b01d8e8932
decagon <udemekendrick@gmail.com>
2023-02-13 15:37:57 +0100

    Landing_Page

---
+++ pay-buddy-frontend/pay-buddy-react-app/src/pages/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h4>THICC MEMES INC</h4>
+            <ul className="list-unstyled">
+              <li>342-420-6969</li>
+              <li>Moscow, Russia</li>
+              <li>123 Streeet South North</li>
+            </ul>
+          </div>
+          {/* Column2 */}
+          <div className="col">
+            <h4>Stuff</h4>
+            <ul className="list-unstyled">
+              <li>DANK MEMES</li>
+              <li>OTHER STUFF</li>
+              <li>GUD STUFF</li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div className="col">
+            <h4>WELL ANOTHER COLUMN</h4>
+            <ul className="list-unstyled">
+              <li>DANK MEMES</li>
+              <li>OTHER STUFF</li>
+              <li>GUD STUFF</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className='copy-right'>
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Footer
